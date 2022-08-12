1d3907e3d657cc15a838667c7e0246525d304567
eri.gram <ericagramuglia@tdf.edu.ar>
2022-08-12 18:12:19 -0300

    falta depurar la funcion eliminarTareaIndicada

--- ejercicio3-JS-parte2/main.js
+++ ejercicio3-JS-parte2/main.js
@@ -1,20 +1,45 @@
 'use strict';
 
+// la llamada de los objetos en el html se realizan por  CLASS
 
-let miEntrada = document.querySelector(".myInput"); //va a contener el valor que hay en el input
-let miLista = document.querySelector (".myOl");
+let miEntrada = document.querySelector(".myInput"); //la variable va a contener el valor que hay en el input y lo traemos con el queryselector
+let miLista = document.querySelector (".myOl");  //lo rojo es la clase
+let miIndice = document.querySelector("myInputOrden");
 
-let arregloTareas =[]; //voy a guardar la lista en un arreglo y lo defino al principio como vacio
+
+//let arregloTareas =[]; //voy a guardar la lista en un arreglo y lo defino al principio como vacio
 
 
 let agregarTarea= ()=> {
-    arregloTareas.push(miEntrada.value); //la nueva tarea la voy a poner al final de la lista con "PUSH"
-    let miItem = document.createElement("li");
-    miItem.innerHTML =  miEntrada.value; //vaciar la lista o igualarla a cero.  innerHTML es la propiedad para cacrgar la lista
-    miLista.appendChild(miItem);
-    miEntrada.value='';
-    
+    let miItem = document.createElement("li"); //agregamos a la lista un elemento, creando el elemento del tipo li
+    miItem.innerHTML =  miEntrada.value; // accedemos al valor con innerHTML, le damos el valor q tiene la entrada input y accedemos al valor con .value 
+    miLista.appendChild(miItem); //agregamos un elemento a la lista, un item, en el DOM
+    miEntrada.value='';    //vaciamos la entrada
 }
 
-let botonAgregar=document.querySelector(".addBtn");
-botonAgregar.addEventListener("click",agregarTarea);
+let eliminarUltTarea = () => { 
+     miLista.removeChild(miLista.lastChild); //remueve el último hijo de mi lista.
+}
+
+let eliminarTodasLasTareas = () => {   //funcion que remueve toda la lista
+    miLista.innerHTML = ''; // voy al contenido de la lista y le agrego vacio
+}
+
+function eliminarTareaIndicada() {
+    let valor = miIndice.value;            
+    miLista.removeChild(miLista.children[valor - 1]); //
+
+}
+
+
+let botonAgregar=document.querySelector(".addBtn"); //a;cá traigo el elemento con la clase del boton
+botonAgregar.addEventListener("click",agregarTarea);  // acá el boton escucha el click para poder ir desp a la funcion
+
+let botonEliminarUlt=document.querySelector(".removeBtn");
+botonEliminarUlt.addEventListener("click",eliminarUltTarea);
+
+let botonEliminarTodo=document.querySelector(".removeAllBtn");
+botonEliminarTodo.addEventListener("click",eliminarTodasLasTareas);
+
+let botonEliminarTarea=document.querySelector(".removeOrdenBtn");
+botonEliminarTarea.addEventListener("click",eliminarTareaIndicada);
