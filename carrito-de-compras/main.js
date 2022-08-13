ce917973d198b18531ae3bc1fd0d1937b231f434
eri.gram <ericagramuglia@tdf.edu.ar>
2022-08-12 22:25:00 -0300

    falta depurar la funcion de sumar

--- carrito-de-compras/main.js
+++ carrito-de-compras/main.js
@@ -1,12 +1,37 @@
-let ListaArray: Array();
-function agregarProductoLista();
+'use strict';
 
+let miEntrada = document.querySelector(".myInputProd");
+let miPrecio = document.querySelector(".myInputPrecio");
+let miLista = document.querySelector (".myUl");
+let arregloPrecio=[];
 
-function generateFibonacci (limit)
-    return [0,1,1,2,3,5,8];
-
-const fibSerie = (generateFibonacci);
-let ul=document.getElementById ("Producto");
-fibSerie.array.forEach(element => {
+let agregarProducto = () => {
+    let miItem = document.createElement("li"); //agregamos a la lista un elemento, creando el elemento del tipo li
     
-});
+    miItem.innerHTML =  miEntrada.value; // accedemos al valor con innerHTML, le damos el valor q tiene la entrada input y accedemos al valor con .value 
+    miLista.appendChild(miItem); //agregamos un elemento a la lista, un item, en el DOM
+    miEntrada.value='';    //vaciamos la entrada de producto
+   
+    let ingresoPcio=miPrecio.value;
+    for (let i=0; i<arregloPrecio.length; i++) { 
+        arregloPrecio[i]= ingresoPcio;
+    }
+       miPrecio.value='';    //vaciamos la entrada de precio
+       console.log("mi arreglo de precios es: ",arregloPrecio)
+}
+
+let calcularSumaPrecio = () => {
+    let suma=0;
+    for (let i=0; i<arregloPrecio.length; i++) { 
+       suma= suma + arregloPrecio[i];
+    }
+ //cómo muestro la suma?
+    console.log ("la suma es:  ",calcularSumaPrecio); 
+}
+
+
+let botonAgregarProducto=document.querySelector(".addProdBtn"); //acá traigo el elemento con la clase del boton
+botonAgregarProducto.addEventListener("click",agregarProducto);  // acá el boton escucha el click para poder ir desp a la funcion
+
+let botonCalcularSuma=document.querySelector(".totalBtn");
+botonCalcularSuma.addEventListener("click",calcularSumaPrecio);
