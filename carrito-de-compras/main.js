e36f690f7e0990dd08501da2bdb407c0863c7021
eri.gram <ericagramuglia@tdf.edu.ar>
2022-08-15 00:41:37 -0300

    ya pasé la variable suma en el html per aún no funciona

--- carrito-de-compras/main.js
+++ carrito-de-compras/main.js
@@ -1,37 +1,37 @@
 'use strict';
 
-let miEntrada = document.querySelector(".myInputProd");
+let miEntrada = document.querySelector(".myInputProd"); //trae el elemento del document (boton)
 let miPrecio = document.querySelector(".myInputPrecio");
 let miLista = document.querySelector (".myUl");
 let arregloPrecio=[];
 
 let agregarProducto = () => {
     let miItem = document.createElement("li"); //agregamos a la lista un elemento, creando el elemento del tipo li
-    
     miItem.innerHTML =  miEntrada.value; // accedemos al valor con innerHTML, le damos el valor q tiene la entrada input y accedemos al valor con .value 
     miLista.appendChild(miItem); //agregamos un elemento a la lista, un item, en el DOM
-    miEntrada.value='';    //vaciamos la entrada de producto
-   
-    let ingresoPcio=miPrecio.value;
-    for (let i=0; i<arregloPrecio.length; i++) { 
-        arregloPrecio[i]= ingresoPcio;
-    }
-       miPrecio.value='';    //vaciamos la entrada de precio
-       console.log("mi arreglo de precios es: ",arregloPrecio)
+    miEntrada.value='';    //se vacía o borra la entrada de producto para volver a ingresar
+    
+    arregloPrecio.push = miPrecio.value; //se carga el arreglo con el input
+    
+    miPrecio.value=''; //se vacía o borra la entrada para volver a ingresar 
+    
+    console.log("mi arreglo dentro de funct agregarProducto: ", arregloPrecio);
+    console.log("longitud de mi arreglo dentro de funct agregarProd",arregloPrecio.length);
 }
+console.log("mi arreglo fuera de funct agregarProducto: ", arregloPrecio);
+console.log("longitud de mi arreglo fuera de funct agregarProducto es",arregloPrecio.length);
 
-let calcularSumaPrecio = () => {
-    let suma=0;
-    for (let i=0; i<arregloPrecio.length; i++) { 
-       suma= suma + arregloPrecio[i];
-    }
- //cómo muestro la suma?
-    console.log ("la suma es:  ",calcularSumaPrecio); 
-}
-
-
-let botonAgregarProducto=document.querySelector(".addProdBtn"); //acá traigo el elemento con la clase del boton
+let botonAgregarProducto=document.querySelector(".addProdBtn"); //acá traigo el elemento con la class del boton
 botonAgregarProducto.addEventListener("click",agregarProducto);  // acá el boton escucha el click para poder ir desp a la funcion
 
 let botonCalcularSuma=document.querySelector(".totalBtn");
-botonCalcularSuma.addEventListener("click",calcularSumaPrecio);
+botonCalcularSuma.addEventListener("click",()=> {
+    let suma = 0;
+    for (let i = 0; i < arregloPrecio.length; i++) {    //realiza la funcion de suma
+        suma = suma + arregloPrecio[i];
+    }
+    document.getElementById('resultadoSuma').innerHTML=suma; //asigno el valor de suma al ID de P para q se escriba en p
+    
+    console.log("mi arreglo desp del click", arregloPrecio);
+    console.log("longitud de mi arreglo desp del click",arregloPrecio.length);
+});
