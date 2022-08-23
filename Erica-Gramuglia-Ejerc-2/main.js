63d78f211930c12e571893f7a19f940a788c2aaf
eri.gram <ericagramuglia@tdf.edu.ar>
2022-08-22 22:52:08 -0300

    examen 22 de agosto

---
+++ Erica-Gramuglia-Ejerc-2/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let boton = document.querySelector(".btnCant");
+boton.addEventListener('click', crearDiv);
+
+function crearDiv () {
+    let miCant = document.querySelector(".cantDiv");
+    miCant = document.createElement("div");
+
+}
--- checkbox/main.js
+++ checkbox/main.js
@@ -1,26 +1,25 @@
-let btn = document.getElementById("button"); //busca el boton en html
-btn.addEventListener("click",saludo); //cuando escucha click ejecuta la funcion saludar
+'use strict';
+function crearH() {
+  let miH1Ok = document.createElement("h1"); //creo el elemento h1
+  let miH2NoOk = document.createElement("h2"); // creo el elemento h2
 
-function saludo (){
-    let contenido=document.getElementById
+  miH1Ok.innerHTML = "El checkbox está seleccionado"; //agrego contenido al h1
+  miH2NoOk.innerHTML = "El checkbox NO está seleccionado" ; // agrego contenido al h2
+}
+function validarCheckbox (){
+let checked = OpCheckBox.checked;
 
-
-   let h1SaludoOk = document.create("h1"); //creo el elemento h1
-   let h2SaludoNoOk = document.create ("h2"); // creo el elemento h2
-
-   h1SaludoOk.className("saludoOk"); // le asigno una class
-   h2SaludoNoOk.className("saludoNoOk"); // le asigno una class
-
-   h1SaludoOk.innerHtml  ("Sí está seleccionado el checkBox"); //agrego contenido
-   h2SaludoNoOk.innerHtml ("No está seleccionado el ckeckBox") //agrego contenido
-    console.log(h1SaludoOk)
-
-    if (document.getElementById ("#checkbox").checked){
-      alert (h1SaludoOk);
-      console.log(h1SaludoOk)
-    }
-    else {
-        alert(h2SaludoNoOk);
-        console.log(h2SaludoNoOk);
+    if (checked) {
+      alert (miH1Ok);
+    } else {
+        alert(miH2NoOk);   
     }
 }
+
+
+
+let btn = document.getElementById("button"); //busca el boton en html
+btn.addEventListener("click",crearH); //cuando escucha click ejecuta la funcion 
+
+let OpCheckBox = document.querySelector(".checkbox");
+OpCheckBox.addEventListener("change", validarCheckbox, false);
